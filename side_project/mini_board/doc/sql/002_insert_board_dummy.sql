580da829b90790df72da9431ee941b80b815ef98
haem1n2 <uehek1234@gmail.com>
2024-03-25 17:29:31 +0900

    240325 웹페이지예제

--- side_project/mini_board/doc/sql/002_insert_board_dummy.sql
+++ side_project/mini_board/doc/sql/002_insert_board_dummy.sql
@@ -2,7 +2,6 @@ USE mini_board;
 
 INSERT INTO boards(title, content)
 VALUES
-
 ("제목1", "내용1")
 ,("제목2", "내용2")
 ,("제목3", "내용3")
---
+++ side_project/mini_board/doc/src/header.php
@@ -0,0 +1,41 @@
+<div class="snowflakes" aria-hidden="true">
+        <div class="snowflake">
+          <div class="inner">🎁</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">🛑</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+        <div class="snowflake">
+          <div class="inner">❅</div>
+        </div>
+      </div>
+    <header>
+        <h1><a href="./list.php?page=<?php echo $prev_page_num ?>">mini Board</a></h1>
+    </header>
