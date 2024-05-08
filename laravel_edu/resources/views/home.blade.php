169823ee9fe0eca9af303d94007c639293230a47
haem1n2 <uehek1234@gmail.com>
2024-05-08 17:02:22 +0900

    240508 laravel start - route

---
+++ laravel_edu/resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>home</h1>
+    <form action="/home" method="post">
+        {{-- csrf-token 추가 --}}
+        @csrf
+        <button type="submit">POST</button>
+    </form>
+    <form action="/home" method="post">
+        @csrf
+        @method('put')
+        <button type="submit">put</button>
+    </form>
+    <br>
+    <form action="/home" method="post">
+    @csrf
+    @method('delete')
+    <button type="submit">delete</button>
+    </form>
+</body>
+</html>
