f4fef20f203b156bd97e94d43c410e86ffde140b
haem1n2 <uehek1234@gmail.com>
2024-05-10 12:52:24 +0900

    240510 라라벨 이론 끝

--- laravel_edu/app/Http/Controllers/UserController.php
+++ laravel_edu/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -65,9 +66,112 @@ class UserController extends Controller
         ->get();
 
     // where 나머지 부터 ... 커밍 순 
-    return var_dump($result);
 
+
+    //select * from users where id in (?,?); [2,5]
+    $result = DB::table('users')
+    ->whereBetween('id',[2,5])
+    ->get();
+
+    // select * from users where deleted_at is null;
+    $result = DB::table('users')
+            ->whereNull('deleted_at')
+            ->get();
+
+    // 2023년에 가입한 사람만 출력
+    // select * from users where year(created_at) = ?
+    // select * from users created_at beetween 20230101000000 and 202312311235959
+    $result = DB::table('users')
+            ->whereYear('created_at', '2023')
+            ->get();
+    
+
+    // 성별회원수
+    $result = DB::table('users')
+            ->select('gender', DB::raw('COUNT(gender) cnt'))
+            ->groupBy('gender')
+            ->having('gender', '=', 'M')
+            ->get();
+    
+    // select id, name from users order by id limit ? offset 2; [1, 2]
+    // $result = DB::table('users')
+    //             ->select('id', 'name')
+    //             ->orderBy('id',)
+    //             ->limit(1)
+    //             ->offset(2)
+    //             ->get();
+    // $reqData = ''; // 유저가 1또는 빈값인 데이터 전달
+    // $result = DB::table('users')
+    //             ->when($reqData, function($query, $reqData){
+    //                 $query->where('id', $reqData);
+    //             })
+    //             // ->dd();
+
+
+    // first() : 쿼리 결과에서 가장 첫번째 레코드만 변환 
+    $result = DB::table('users')->first();
+    // count() : 쿼리 결과의 레코드 수를 반환
+    $result = DB::table('users')->count();
+    //find($id) : 지정된 기본키의 해당하는 레코드를 반환
+    $result = DB::table('users')->find(3);
+    
+
+    // insert 
+    // $result = DB::table('users')->insert([
+    //     'name' => '김영희'
+    //     ,'email' => 'kim@admin.com'
+    //     ,'password' => Hash::make('qwer1234!')
+    //     ,'gender' => 'F'
+    // ]);
+
+    // update 
+    // $result = DB::table('users')
+    //     ->where('id', 8)
+    //     ->update([
+    //     'email' => 'kim@naver.com'
+    // ]);
+    
+    // // delete
+    // $result = DB::table('users')
+    //             ->where('id', 8)
+    //             ->delete();
+    
+    
+    // ---------------------
+    // Eloquent Model
+    // ---------------------
+    $result = User::all(); 
+    //  $result = User::find(3);
+    
+    // insert
+    $data = [
+        'name' => '김영희'
+        ,'email' => 'kim@naver.com'
+        ,'password' => Hash::make('qwer1234!')
+        ,'gender' => 'F'
+    ];
+    // $result = User::create($data);
+
+    // update
+    // DB::beginTransaction();
+    // $target = User::find(14);
+    // $target->gender = 'M';
+    // $result = $target->save();
+    // // DB::commit();
+
+    // // delete
+    // $result = User::destroy(14);
+
+    // 소프트 딜리트 된 데이터 조회
+    $result = User::withTrashed()->get(); // 소프트 딜리트 포함
+    $result = User::onlyTrashed()->get(); // 소프트 딜리트만
+    
+
+    // 소프트 딜리트 된거 복원
+    $result = User::where('id', 14)->restore();
+    return var_dump($result);
     } 
+    
 }
 
 
--- laravel_edu/app/Models/User.php
+++ laravel_edu/app/Models/User.php
@@ -4,24 +4,40 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    // 해당 모델에 소프트딜리트를 적용시키고 싶으면 softDeletes 트레이트 추가 
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    
+    // 모델과 이어질 테이블 명을 정의하는 프로퍼티
+    // protected $table ='users';
+
+    // pk를 지정하는 프로퍼티
+    // protected $primaryKey = 'id';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
+     // upsert 허용할 컬럼들 설정하는 프로퍼티(화이트 리스트)
     protected $fillable = [
         'name',
         'email',
         'password',
+        'gender'
     ];
+
+    // upsert시 비허용할 컬럼들 설정하는 프로퍼티 (블랙 리스트)
+    // protected $guarded = [
+    //   'id'
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
