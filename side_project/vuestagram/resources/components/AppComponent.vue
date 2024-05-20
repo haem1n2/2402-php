000a5937cd4198e8a9652ca5819b121583c1c0f7
haem1n2 <uehek1234@gmail.com>
2024-05-20 17:21:50 +0900

    240520 뷰 라라벨

---
+++ side_project/vuestagram/resources/components/AppComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- header -->
+    <header>
+        <div class="header-container">
+            <div class="header-content">
+                <div class="title">
+                    <router-link to="/"><h1>Vuestagram</h1></router-link>
+                </div>
+                <img class="img-logo" src="/1.png" alt="">
+                <div class="btn-group">
+                    <div v-if="true">
+                    <router-link to="/login"><button class="btn btn-header btn-bg-black">로그인</button></router-link>
+                    <router-link to="/login"><button class="btn btn-header btn-bg-white">가입하기</button></router-link>
+                    </div>
+                    <div v-else>
+                    <button class="btn btn-header btn-bg-white">로그아웃</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <hr>
+<!-- main -->
+    <main>
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </main>
+<!-- footer -->
+    <footer>
+        <p>카피라이트인데 귀찮아죽겟음 응 안해 수고</p>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@import url('../css/common.css');
+</style>
---
+++ side_project/vuestagram/resources/components/BoardComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>보드컨트롤ㄹ러</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
